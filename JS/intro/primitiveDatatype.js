ffdd71cdb6d559baa34836caf6315dc37a4a5849
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-06-17 15:41:27 +0300

    learnt about datatypes

---
+++ JS/intro/primitiveDatatype.js
@@ -0,0 +1,29 @@
+let number1=23;
+console.log(number1);
+let number2=34.34;
+console.log(number2);
+let number3=-34;
+console.log(number3);
+
+let str1="Double \n quote I am patricia"//double quotes
+console.log(str1)
+let str2='Single Quote I am patricia'//Single quote
+console.log(str2)
+let str3=`Back Ticks I am patricia `//Back Ticks
+console.log(str3)
+let str4="23" //string or a number:<String>
+console.log("23 is a string Don't")
+
+let johnIsMale=true;
+console.log(johnIsMale);
+let johnIsMarried=false;
+console.log(johnIsMarried);
+
+let nullval1=null;
+console.log(nullval1);
+
+let undefinedvalue1=undefined
+console.log(undefinedvalue1);
+
+let undefinedvalue2;
+console.log(undefinedvalue2);
