8f28a6340631af6e0b3ef5cbdb56193aca8643b1
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-06-18 15:00:36 +0300

    learnt string interpolation

---
+++ JS/intro/templateString.js
@@ -0,0 +1,13 @@
+let firstName="Patricia"
+let secondName="Muthoni"
+let age=20
+let address="juja county"
+
+let userDetails=firstName+" "+secondName+" is "+age+" years old "+"he stays at"+address
+console.log(userDetails)
+console.log(" User details is ",typeof userDetails)
+
+let userDetails2=
+`${firstName} ${secondName} is ${age} years old he stays at ${address}`;
+console.log(userDetails2)
+console.log(`${firstName} ${secondName} is ${age} years old he stays at ${address}`)
