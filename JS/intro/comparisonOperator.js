a7638055b7321c570e4f49765478d67dbc639d6f
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-06-19 10:09:16 +0300

    learnt comparison operators

---
+++ JS/intro/comparisonOperator.js
@@ -0,0 +1,26 @@
+let numb1="28";
+let numb2=29;
+let strictlyEqual=numb1===numb2;
+
+console.log(`numb1=${numb1}===numb2=${numb2}
+     Ans ${strictlyEqual});its type is ${typeof strictlyEqual}`)
+
+     let nonStrictEquality=numb1==numb2;
+     console.log(`numb1=${numb1}==numb2=${numb2}
+     Ans ${nonStrictEquality});its type is ${typeof nonStrictEquality}`)
+     
+
+     let val1="hello"
+     let val2="how are you"
+
+     let strictInequality=val1!==val2
+     console.log(`val1=${val1}!==val2=${val2}
+     Ans ${strictInequality});its type is ${typeof strictInequality}`)
+
+     let inequality=val1!=val2
+     console.log(`val1=${val1}!=val2=${val2}
+     Ans ${inequality});its type is ${typeof inequality}`)
+
+     
+
+
