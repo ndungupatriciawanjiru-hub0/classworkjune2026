1a487269486e973f0c8549633e553945c321ea25
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-06-22 10:09:33 +0300

    completed comparison operators

--- JS/intro/comparisonOperator.js
+++ JS/intro/comparisonOperator.js
@@ -21,6 +21,23 @@ console.log(`numb1=${numb1}===numb2=${numb2}
      console.log(`val1=${val1}!=val2=${val2}
      Ans ${inequality});its type is ${typeof inequality}`)
 
-     
+    let numb3=20;
+     let numb4=10
 
+     let str1="a"
+     let str2="b"
 
+console.log(`num3=${numb3} and numb4=${numb4}`)
+console.log(`str1=${str1} and str2=${str2}`)
+
+console.log(`numb3>numb4 ${numb3>numb4}`)
+console.log(`str2>str1 ${str1>str2}`)
+
+console.log(`numb3>=numb4 ${numb3>=numb4}`)
+console.log(`str2>=str1 ${str1>=str2}`)
+
+console.log(`numb3<numb4 ${numb4>numb3}`)
+console.log(`str2<str1 ${str1<str2}`)
+
+console.log(`numb3<=numb4 ${numb3<=numb4}`)
+console.log(`str2<=str1 ${str1<=str2}`)
