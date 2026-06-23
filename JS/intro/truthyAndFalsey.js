ecf0277e595b8ef6153ab638d2d3da8da6d93534
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-06-23 16:02:05 +0300

    looked at truthyAndFalsey

---
+++ JS/intro/truthyAndFalsey.js
@@ -0,0 +1,27 @@
+/*
+Truthy or falsey
+->If you know something is not falsey
+then it is truthy.
+falsey
+1.false
+2.0
+3.-0
+4.null
+5.undefined
+6.''->empty string
+7.NaN->not a number
+Anything that is not falsey is truthy
+| truthy|| falsey -> truthy
+
+*/
+
+let userName1="Patricia Ndung'u" //truthy
+let userName2=0//falsey
+
+let activeUserName=userName2 || userName1
+
+
+console.log(activeUserName)
+
+let bothUserNameOk=userName1&&userName2
+console.log(bothUserNameOk)//truthy or falsey
