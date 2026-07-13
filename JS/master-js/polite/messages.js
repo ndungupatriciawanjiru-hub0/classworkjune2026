4b668b6580710473c9e8ba161f9299001b09d994
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-07-13 16:54:25 +0300

    generated applicant rejection messages and wrote to output.json

--- JS/master-js/polite/messages.js
+++ JS/master-js/polite/messages.js
@@ -1,80 +1,79 @@
-const fs = require('fs');
+//import data
 
+/*
+Node js<fs>
+for your data in the .json file.
+for each applicant data.
+create a message: to inform him/her
+that he/she did not get the job.
+<meet qualificatino>
+but be on the lookout for
+new oprtunities from us in the future.
+
+*/
 /*
 Step 1. ensure your js file can access the data.
 */
 const data = require("./data.json");
-console.log(data);
+const fs = require("fs");
 
 /*
-Step 2: use a for/while loop to go through each data point
+Step 2:use a for<while> a loop to go through each data point
 -print the data
-*/
-let i = 0;
-while (i < data.length) {
-    console.log(data[i]);
-    i++;
-}
 
-/*
-Step 3: for each data point create a message example
+Step 3:for each data point create a message example
 'Dear <<sir/madam>based on gender>>
-We regret to inform you that you wount be going
-to next stage of the interview.
-the reasone.you not a good fit.
-'
-*/
-function createMessage(person) {
-    const salutation = person.gender === 'Male' ? 'sir' : 'madam';
-    const message = `Dear ${salutation},
-We regret to inform you that you wount be going
-to next stage of the interview.
-the reasone.you not a good fit.
-`;
-    console.log(message);
-    return message;
-}
+  We regret to inform you that you wount be going
+  to next stage of the interview.
+  the reasone.you not a good fit.
+ '
+  console.log(the message)
 
-i = 0;
-while (i < data.length) {
-    createMessage(data[i]);
-    i++;
-}
-
-/*
-Step 4: create an empty array and fill it with
+Step 4:create an empty array and
 the user data including the messages.
 output object should be
-{
-  "id": 4,
-  "first_name": "Krystle",
-  "last_name": "Rubroe",
-  "email": "krubroe3@indiatimes.com",
-  "gender": "Female",
-  "phone": "244-641-3793",
-  "message": <>
-}
+ {
+    "id": 4,
+    "first_name": "Krystle",
+    "Last_name": "Rubroe",
+    "email": "krubroe3@indiatimes.com",
+    "gender": "Female",
+    "phone": "244-641-3793",
+    "message":<>
+  }
+
+Step 5:write this data to a file called output.json.
+  Hint do JSON.stringify ->  js object to json notation.
+  save this data to your file ie output.json
 */
-const output = [];
-i = 0;
-while (i < data.length) {
-    const person = data[i];
-    output.push({
-        id: person.id,
-        first_name: person.first_name,
-        last_name: person.last_name,
-        email: person.email,
-        gender: person.gender,
-        phone: person.phone,
-        message: createMessage(person)
-    });
-    i++;
-}
 
 /*
-Step 5: write this data to a file called output.json.
-Hint do JSON.stringify -> js object to json notation.
-save this data to your file ie output.json
+ for-> async->
+ -map <method>
 */
-fs.writeFileSync('./output.json', JSON.stringify(output, null, 2));
-console.log('output.json has been created!');
+
+const output = [];
+
+for (let i = 0; i < data.length; i++) {
+  const doc = data[i];
+  //
+  //   const first_name = doc.first_name;
+  //   const last_name = doc.last_name;
+  //   const full_name=doc.first+doc.last_name
+  const { first_name, last_name, email, gender, phone } = doc;
+
+  let title = "";
+  if (doc.gender === "Male") {
+    title = "Sir";
+  } else {
+    title = "Madam";
+  }
+  const message = `
+  Dear ${title}, ${doc.first_name} ${last_name}. We regret to inform you
+  will not procede in the next round of interviews.
+  `;
+  output.push({ first_name, last_name, email, gender, phone, message });
+}
+
+const outputText = JSON.stringify(output); //text
+fs.writeFile("output.json", outputText, (error) => {});
