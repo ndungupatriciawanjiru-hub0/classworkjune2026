39786b156b93de0ac60e5aace45808bae8fc0dd2
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-08-01 09:12:16 +0300

    Added  props and array mapping exercises

--- REACT/vite-project/src/Props/index.jsx
+++ REACT/vite-project/src/Props/index.jsx
@@ -12,22 +12,6 @@
   including other components
 */
 
-function Parent() {
-  //primitive datatypes
-  const str = "Hello world"; ///string
-  const amount = 2000; //number
-  const isOk = true; //boolean
-  const notSet = null; //null
-
-  return (
-    <div>
-      <h1> I am the parent component</h1>
-      <Child str={str} amountInNumber={amount} isOk={isOk} notSet={notSet} />
-      <Child1 str={str} amountInNumber={amount} isOk={isOk} notSet={notSet} />
-    </div>
-  );
-}
-
 /*
 Child: receives props but doesn't display them in JSX,
 only logs to console. Demonstrates that props exist
@@ -64,14 +48,123 @@ function Child1(props) {
         </li>
         <li>
           isOk <b>{String(props.isOk)}</b>{" "}
-          {/*
-            for booleans convert them to string before displaying
-            */}
+          {/* for booleans convert them to string before displaying */}
         </li>
         <li>
           Not set <b>{props.notSet}</b>
         </li>
       </ul>
+    </div>
+  );
+}
+
+// display props using prop keyword
+function Child1b(props) {
+  return (
+    <div>
+      <h1>I am the child 1 component</h1>
+      <ul>
+        <li>
+          Favourite string <b>{props.str}</b>
+        </li>
+        <li>
+          Amount <b>{props.amountInNumber}</b>
+        </li>
+        <li>
+          isOk <b>{props.isOk}</b>
+        </li>
+        <li>
+          isOk <b>{String(props.isOk)}</b>{" "}
+          {/* for booleans convert them to string before displaying */}
+        </li>
+        <li>
+          Not set <b>{props.notSet}</b>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+// using destructuring
+function Child2(props) {
+  /*
+  key:value
+    const str=props.str
+    const amountInNumber=props.amountInNumber
+    const amount=props.amountInNumber
+  key:number<>
+  key:<string variable naming> "hi boss":34
+    const {str,amountInNumber}=props
+  */
+  const { str, amountInNumber, isOk, notSet } = props;
+  return (
+    <div>
+      <h1>I am the child 2 component</h1>
+      <ul>
+        <li>
+          Favourite string <b>{str}</b>
+        </li>
+        <li>
+          Amount <b>{amountInNumber}</b>
+        </li>
+        <li>
+          isOk <b>{isOk}</b>
+        </li>
+        <li>
+          isOk <b>{String(isOk)}</b>{" "}
+          {/* for booleans convert them to string before displaying */}
+        </li>
+        <li>
+          Not set <b>{notSet}</b>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+// display props using prop keyword
+// direct destructuring
+function Child2b({ str, amountInNumber, isOk, notSet }) {
+  return (
+    <div>
+      <h1>I am the child 2 component</h1>
+      <ul>
+        <li>
+          Favourite string <b>{str}</b>
+        </li>
+        <li>
+          Amount <b>{amountInNumber}</b>
+        </li>
+        <li>
+          isOk <b>{isOk}</b>
+        </li>
+        <li>
+          isOk <b>{String(isOk)}</b>{" "}
+          {/* for booleans convert them to string before displaying */}
+        </li>
+        <li>
+          Not set <b>{notSet}</b>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Parent() {
+  //primitive datatypes
+  const str = "Hello world"; ///string
+  const amount = 2000; //number
+  const isOk = true; //boolean
+  const notSet = null; //null
+
+  return (
+    <div>
+      <h1> I am the parent component</h1>
+      <Child str={str} amountInNumber={amount} isOk={isOk} notSet={notSet} />
+      <Child1 str={str} amountInNumber={amount} isOk={isOk} notSet={notSet} />
+      <Child1b str={str} amountInNumber={amount} isOk={isOk} notSet={notSet} />
+      <Child2 str={str} amountInNumber={amount} isOk={isOk} notSet={notSet} />
+      <Child2b str={str} amountInNumber={amount} isOk={isOk} notSet={notSet} />
     </div>
   );
 }
