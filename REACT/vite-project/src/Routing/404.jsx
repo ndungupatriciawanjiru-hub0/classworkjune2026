be3d03d917558cf214f3afda24c61d83b133f680
PNDUNGU-DEV <ndungu.patriciawanjiru@gmail.com>
2026-08-11 10:31:18 +0300

    Added Navigation1 (NavLink) and Navigation2 (useNavigate/useLocation) components with active-route styling, and auto-redirect countdown on CrazyRoute

--- REACT/vite-project/src/Routing/404.jsx
+++ REACT/vite-project/src/Routing/404.jsx
@@ -1,15 +1,14 @@
-import { Link } from "react-router";
+import Navigation1 from "./Navigation1";
 
 function Page404() {
   return (
     <div>
+      <Navigation1 />
       <h1>
         <b>404</b>
       </h1>
       <h1>We cant find whatever you are looking for.</h1>
-      <h1>
-        <Link to="/">Go back home</Link>
-      </h1>
+      <h1>Go back home</h1>
     </div>
   );
 }
--- REACT/vite-project/src/Routing/About.jsx
+++ REACT/vite-project/src/Routing/About.jsx
@@ -1,7 +1,12 @@
+import Navigation1 from "./Navigation1";
+import Navigation2 from "./Navigation2";
+
 function About() {
   return (
     <div>
-      <h1>This is the about page</h1>
+      <Navigation1 />
+      <Navigation2 />
+      <h1>This is the about us page</h1>
     </div>
   );
 }
