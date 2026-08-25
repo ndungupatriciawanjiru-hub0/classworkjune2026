2fbcad669cb3483a1d40b220b9b7d46d5f7f7218
Patricia Ndung'u <ndungu.patriciawanjiru@gmail.com>
2026-08-25 10:37:22 +0300

    Intro to Python: variables, data types, and basic operators

---
+++ PYTHON/data_type.py
@@ -0,0 +1,20 @@
+# Example variables
+x = 10          # Integer
+y = 3.14        # Float
+z = "Hello"     # String
+a = True        # Boolean<True, False>
+b = [1, 2, 3, 4]   # List <Array> Mutable <By value>
+c = {1, 2, 3}      # Set
+d = (1, 2, 3)      # Tuple <List> Immutable
+e = {"key": "value"}   # Dictionary <Object:js>
+# for dictionary use Bracket notation
+
+# Determining the types
+print("X is ", x, "Its type ", type(x))   # Output: <class 'int'>
+print(f"Y is {y} its type is {type(y)}")  # Output: <class 'float'>
+print(type(z))   # Output: <class 'str'>
+print(type(a))   # Output: <class 'bool'>
+print(type(b))   # Output: <class 'list'>
+print(type(c))   # Output: <class 'set'>
+print(type(d))   # Output: <class 'tuple'>
+print(type(e))   # Output: <class 'dict'>
