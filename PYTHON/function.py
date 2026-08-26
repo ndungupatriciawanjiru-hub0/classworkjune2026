8f7156429958684b0e9c335307f5848b214a6470
Patricia Ndung'u <ndungu.patriciawanjiru@gmail.com>
2026-08-26 10:38:27 +0300

    Added condition_and_loop and functions practice files

---
+++ PYTHON/function.py
@@ -0,0 +1,15 @@
+# function areaCircle(radius)
+# { return 3.142*radius*radiues}
+
+# def -> pi
+
+def area_cirlce(radius):
+    ans = 3.142 * pow(radius, 2)
+    print(f"for circle of radius {radius} area is {ans}")
+    return
+
+area_cirlce(20)
+
+new_f = area_cirlce
+
+new_f(10)
