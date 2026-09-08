ec61de367ce4ce0ebea975882706fbeb87e5c7aa
Patricia Ndung'u <ndungu.patriciawanjiru@gmail.com>
2026-09-08 10:52:53 +0300

    Added  Shape polymorphism and inheritance classwork

--- PYTHON/object_oriented_programming/with_inheritance.py
+++ PYTHON/object_oriented_programming/with_inheritance.py
@@ -1,6 +1,9 @@
 """
 Shape class.
     properties and methods common to all shapes.
+
+    --> private , public <--
+    --> getters and setters hide the properties <private>
 """
 
 class Shape:
@@ -17,6 +20,13 @@ class Shape:
         print(f"Perimeter: {self.perimeter()}")
         print("---------------------------------------")
 
+    #method area
+    def area(self):
+        print(f"For shape {self.shape_name} area calculation missing")
+
+    def perimeter(self):
+        print(f"For shape {self.shape_name} perimeter calculation missing")
+
 
 #inheritance class Name (<class inheriting from>)
 class Rectangle(Shape):
@@ -30,6 +40,7 @@ class Rectangle(Shape):
         self.length=length
         self.width=width
 
+    #method calculating area
     def area(self):
         return self.length*self.width
 
@@ -37,10 +48,33 @@ class Rectangle(Shape):
         return 2*(self.length+self.width)
 
 
-r1=Rectangle(length=20,width=12)
+class Triangle(Shape):
 
-print("Shape name",r1.shape_name) #shape name
-print("Area is",r1.area()) #Recange.area <>
+    def __init__(self):
+        self.base.height;
+        super().__init__(shape_name="Triangle")
 
-r1.describe() #Shape
-r1.display_info() #shape
+
+#code for the square class
+class Square(Rectangle):
+    def __init__(self,side):
+        #super -> its not shape its Rectangle
+        super().__init__(length=side,width=side)
+        self.shape_name="Square"
+
+
+s1=Square(side=35)
+print("Shape name",s1.shape_name) #shape name
+print("Area is",s1.area()) #Square -> Rectangle -> Shape
+s1.describe() #Shape
+s1.display_info() #shape
+
+# r1=Rectangle(length=20,width=12)
+
+# print("Shape name",r1.shape_name) #shape name
+# print("Area is",r1.area()) #Recange.area <>
+# r1.describe() #Shape
+# r1.display_info() #shape
+
+t1=Triangle(20,30)
+t1.area() # Triangle -> Shape
